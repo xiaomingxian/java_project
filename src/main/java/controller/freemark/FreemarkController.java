48727a824d2d8c853a92c19b4a875647830b1e5b
xiaoming.xian <xiaoming.xian@cintel.com.cn>
2019-01-11 17:51:17 +0800

    freemark

--- src/main/java/controller/freemark/FreemarkController.java
+++ src/main/java/controller/freemark/FreemarkController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("freemark")
 public class FreemarkController {
 
 
     @RequestMapping("getPage")
     public ModelAndView getPage() {
-        ModelAndView mv = new ModelAndView("getPage.ftl");
+        ModelAndView mv = new ModelAndView("getPage");
         return mv;
 
     }
