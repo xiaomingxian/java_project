5920e0d8133d8108aa4382f96622671f68aa27d8
xxm <934792834@qq.com>
2019-04-15 00:33:53 +0800

    --------------------jvm 堆内存快照，jconsole

---
+++ src/test/java/jvm/pojo/JconsolePojo.java
@@ -0,0 +1,4 @@
+package jvm.pojo;
+
+public class JconsolePojo {
+}
---
+++ src/test/java/jvm/test/T1_JconsoleTest.java
@@ -0,0 +1,37 @@
+package jvm.test;
+
+import jvm.pojo.JconsolePojo;
+
+import java.util.ArrayList;
+
+public class T1_JconsoleTest {
+    //  增大对象所占内存到128k  public防止垃圾回收器回收？why   理论上堆内存一直上升
+    public byte[] b = new byte[128 * 1024];
+
+    //public T1_JconsoleTest(){
+    //     //作为局部变量可被回收
+    //     byte[] b = new byte[128 * 1024];
+    //
+    //}
+
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        full(1000);
+    }
+
+    private static void full(int size) {
+        ArrayList<Object> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(new JconsolePojo());
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
