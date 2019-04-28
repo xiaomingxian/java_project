5fea05eb4b6897e6e59a36a5fc0ec985062e4535
xxm <934792834@qq.com>
2019-04-28 22:52:02 +0800

    nio 缓冲区，通道

---
+++ src/test/java/interview/nio/test/T1_Base.java
@@ -0,0 +1,104 @@
+package interview.nio.test;
+
+import org.junit.Test;
+import org.python.netty.buffer.ByteBufUtil;
+
+import java.nio.ByteBuffer;
+
+public class T1_Base {
+    /**
+     * 1.缓冲区[Buffer]: 在NIO中负责存储数据，缓冲区是数组，用于存储不同类型的数据
+     * 根据类型不同提供了相应的缓冲区[boolean除外]
+     * ByteBuffer
+     * CharBuffer
+     * ShortBuffer
+     * IntBuffer
+     * LongBuffer
+     * FloatBuffer
+     * DoubleBuffer
+     *
+     *
+     * 上述几乎都是通过allocate()获取缓冲区
+     *
+     * 2.put()  存
+     *  get()    取
+     *
+     *  3.0<=mark<=postion<=limit<=capcity
+     *
+     *  4.直接缓冲区[物理映射文件]，非直接缓冲区[jvm中]
+     */
+
+    /**
+     * 核心属性
+     * capacity 容量-一旦声明不可改变
+     * limit    表示缓冲区中可以操作的数据的大小，limit后不可读写
+     * position 当前位置
+     */
+    @Test
+    public void t1() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        System.out.println("-----------存放数据-----------");
+        byteBuffer.put(new byte[5]);
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        System.out.println("-----------读取数据-----------");
+        //先转换为读
+        byteBuffer.flip();
+        //读取三次
+        //byteBuffer.get();
+        //byteBuffer.get();
+        //byteBuffer.get();
+        //读取指定长度
+        byte[] size = new byte[byteBuffer.limit()];
+        byteBuffer.get(size, 0, 3);
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        System.out.println("----------重复读数据 rewind --------");
+        byteBuffer.rewind();
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        System.out.println("----------清空缓冲区 clear  ---------");
+        //但缓冲区的数据还存在，处于被遗忘状态----只是核心属性的指针回到了原始状态
+        byteBuffer.clear();
+        System.out.println(byteBuffer.capacity());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+
+        System.out.println("数据没有被清除还可以被读取到：" + byteBuffer.get());
+
+    }
+
+    @Test
+    public void mark() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        byteBuffer.put("abcde".getBytes());
+        //切换到读状态
+        byteBuffer.flip();
+        System.out.println("-------------- mark reset后恢复到mark位置 ---------------");
+        System.out.println(byteBuffer.position());
+        //读取几个-再记录位置
+        byte[] size = new byte[byteBuffer.limit()];
+        byteBuffer.get(size, 0, 2);
+        System.out.println(byteBuffer.position());
+        byteBuffer.mark();
+        byteBuffer.get(size, 2, 3);
+        byteBuffer.reset();
+
+        System.out.println(byteBuffer.position());
+
+
+    }
+
+    @Test
+    public void direct(){
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        boolean direct = byteBuffer.isDirect();
+        System.out.println(direct);
+    }
+}
---
+++ src/test/java/interview/nio/test/T2_Channel.java
@@ -0,0 +1,140 @@
+package interview.nio.test;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class T2_Channel {
+    /**
+     * 用传统的输入流输出流获取通道
+     * jvm内存demo,也可以直接内存
+     */
+    @Test
+    public void t1_copy() {
+        try {
+            FileInputStream in = new FileInputStream("/Users/xxm/Desktop/3.直接缓冲区与非直接缓冲区.jpg");
+            FileOutputStream out = new FileOutputStream("/Users/xxm/develop/workspace/learn/src/test/java/interview/nio/pic/3.直接缓冲区与非直接缓冲区.jpg");
+
+            //1.获取通道
+            FileChannel inChannel = in.getChannel();
+            FileChannel outChannel = out.getChannel();
+
+            //2. 指定缓冲区大小
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while (inChannel.read(buffer) != -1) {
+                //切换为读模式
+                buffer.flip();
+                //写数据
+                outChannel.write(buffer);
+                //    清空缓冲区
+                buffer.clear();
+            }
+
+            //    关闭通道，流--正确写法写在finally 并做判断
+            outChannel.close();
+            inChannel.close();
+            in.close();
+            out.close();
+            System.out.println("--->复制成功");
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * File工具获取通道
+     * 直接缓冲区demo
+     */
+    @Test
+    public void fileUtils() {
+
+        try {
+            //读取通道------------------可变参，多个拼接------------- 枚举类[读还是写]
+            FileChannel readChannel = FileChannel.open(Paths.get("/Users/xxm/Desktop/通道.jpg"), StandardOpenOption.READ);
+            FileChannel writeChannel = FileChannel.open(Paths.get("/Users/xxm/develop/workspace/learn/src/test/java/interview/nio/pic/4.通道.jpg")
+                    , StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE_NEW);//不存在就创建，存在就报错---可变参[多种模式]
+            //内存映射文件---直接内存
+            MappedByteBuffer in = readChannel.map(FileChannel.MapMode.READ_ONLY, 0, readChannel.size());
+            //    写
+            MappedByteBuffer out = writeChannel.map(FileChannel.MapMode.READ_WRITE, 0, readChannel.size());
+
+            //    从通道获取数据--再通过输出通道将数据放入直接内存--通过Channel直接读入磁盘
+            byte[] size = new byte[in.limit()];
+            in.get(size);
+            out.put(size);
+            //
+            readChannel.close();
+            writeChannel.close();
+            System.out.println("复制完毕");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 通道间的 from to
+     */
+    @Test
+    public void channelTrsform() {
+        try {
+
+            FileChannel readChannel = FileChannel.open(Paths.get("/Users/xxm/Desktop/获取通道的几种方式.jpg"), StandardOpenOption.READ);
+            FileChannel writeChannel = FileChannel.open(Paths.get("/Users/xxm/develop/workspace/learn/src/test/java/interview/nio/pic/5.获取通道的几种方式.jpg")
+                    , StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE_NEW);//不存在就创建，存在就报错---可变参[多种模式]
+
+            //readChannel.transferTo(0, readChannel.size(), writeChannel);
+            //或者
+            writeChannel.transferFrom(readChannel, 0, readChannel.size());
+
+            readChannel.close();
+            writeChannel.close();
+            System.out.println("---ok---");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     * 分散，聚集
+     */
+    @Test
+    public void fJ() {
+        try {
+            //分散读取
+            RandomAccessFile rw = new RandomAccessFile("/Users/xxm/develop/workspace/learn/src/test/java/interview/nio/test/T1_Base.java", "rw");
+            FileChannel channel = rw.getChannel();
+            //定义多个缓冲区
+            ByteBuffer b1 = ByteBuffer.allocate(100);
+            ByteBuffer b2 = ByteBuffer.allocate(1024);
+            ByteBuffer[] bs = {b1, b2};
+            channel.read(bs);
+            //    读取每一个缓冲区
+            for (ByteBuffer b : bs) {
+                b.flip();
+            }
+            System.out.println(new String(bs[0].array(), 0, bs[0].limit()));
+            System.out.println("----------------");
+            System.out.println(new String(bs[1].array(), 0, bs[1].limit()));
+
+            //     聚集
+            RandomAccessFile write = new RandomAccessFile("/Users/xxm/Desktop/1.java", "rw");
+            FileChannel channel1 = write.getChannel();
+            channel1.write(bs);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
