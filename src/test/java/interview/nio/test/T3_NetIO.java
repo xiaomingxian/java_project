b35537de32027ce8df2f57415e493fc5caa600c7
xxm <934792834@qq.com>
2019-04-29 00:06:16 +0800

    nio 网络传输 阻塞式

---
+++ src/test/java/interview/nio/test/T3_NetIO.java
@@ -0,0 +1,57 @@
+package interview.nio.test;
+
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class T3_NetIO {
+    @Test
+    public void client() throws Exception {
+        //获取socket通道
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("localhost", 9999));
+        //获取本地文件通道
+        FileChannel fileChannel = FileChannel.open(Paths.get("/Users/xxm/develop/workspace/learn/src/test/java/interview/nio/test/T2_Channel.java"), StandardOpenOption.READ);
+        //缓存
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        //    从文件通道读取，通过socket通道写出
+        while (fileChannel.read(buffer) != -1) {
+            //    切换为读模式
+            buffer.flip();
+            socketChannel.write(buffer);
+            //清空缓存
+            buffer.clear();
+        }
+        //    关闭通道
+        fileChannel.close();
+        socketChannel.close();
+
+
+    }
+
+    @Test
+    public void server() throws Exception {
+        //服务端socket通道
+        ServerSocketChannel open = ServerSocketChannel.open();
+        open.bind(new InetSocketAddress(9999));
+        FileChannel open1 = FileChannel.open(Paths.get("aaa.java"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+        //    从socket通道读取，放入文件通道
+        SocketChannel accept = open.accept();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        while (accept.read(buffer) != -1) {
+        //切换为读取模式
+            buffer.flip();
+            open1.write(buffer);
+            buffer.clear();
+        }
+        open1.close();
+        open.close();
+    }
+}
