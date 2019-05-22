a95af8be336494e3989358a2a57c22a6c292af2b
xxm <934792834@qq.com>
2019-05-22 23:53:53 +0800

    java命令排除故障---eg:死锁原因排查

---
+++ src/test/java/interview/t0/mianshi/test/T16_DieLock.java
@@ -0,0 +1,59 @@
+package interview.t0.mianshi.test;
+
+import lombok.Data;
+import org.junit.Test;
+
+public class T16_DieLock {
+
+    @Test
+    public void die() {
+
+        //jps -l 查进程号
+        //jstack 进程号  查异常堆栈信息
+        String A = "A";
+        String B = "222";
+
+        Thread t1 = new Thread(new MyTask(A, B));
+        Thread t2 = new Thread(new MyTask(B, A));
+
+        t1.start();
+        t2.start();
+
+
+
+    }
+
+
+}
+
+@Data
+class MyTask implements Runnable {
+
+    private String A;
+    private String B;
+
+    public MyTask(String A, String B) {
+        this.A = A;
+        this.B = B;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (this.A) {
+                System.out.println("-----111111");
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                }
+                synchronized (this.B) {
+                    System.out.println("====== 2222222");
+                }
+
+            }
+
+        }
+
+
+    }
+}
