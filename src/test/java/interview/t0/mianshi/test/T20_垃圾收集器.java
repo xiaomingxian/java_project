04a3c0139bbade048635dfda36aa5221745058d7
xxm <934792834@qq.com>
2019-05-26 13:24:28 +0800

    垃圾收集器-1

---
+++ src/test/java/interview/t0/mianshi/test/T20_垃圾收集器.java
@@ -0,0 +1,17 @@
+package interview.t0.mianshi.test;
+
+import org.junit.Test;
+
+public class T20_垃圾收集器 {
+
+    /**
+     * -XX:+PrintCommandLineFlags
+     * -XX:InitialHeapSize=268435456 -XX:MaxHeapSize=4294967296 -XX:+PrintCommandLineFlags
+     * -XX:+UseCompressedClassPointers -XX:+UseCompressedOops -XX:+UseParallelGC
+     */
+    @Test
+    public void t1() {
+
+    }
+
+}
