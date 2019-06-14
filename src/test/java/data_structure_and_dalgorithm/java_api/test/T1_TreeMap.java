e3d10048433958a0b449b1c79a57af9957b3850a
xiaoming.xian <xiaoming.xian@cintel.com.cn>
2019-06-14 17:19:29 +0800

    redis过期策略

---
+++ src/test/java/data_structure_and_dalgorithm/java_api/test/T1_TreeMap.java
@@ -0,0 +1,17 @@
+package data_structure_and_dalgorithm.java_api.test;
+
+import org.junit.Test;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class T1_TreeMap {
+
+
+    @Test
+    public void tree() {
+        new TreeMap<>();
+
+        new TreeSet<>();
+    }
+}
---
+++ src/test/java/redis/test/T1_LRU.java
@@ -0,0 +1,8 @@
+package redis.test;
+
+/**
+ * LRU算法
+ * Least Recently Used的缩写，即最近最少使用
+ */
+public class T1_LRU  {
+}
